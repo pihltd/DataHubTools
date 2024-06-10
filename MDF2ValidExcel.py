b2bdc8915e4481c5a850078f802d35b313a87216
Todd Pihl <todd.pihl@gmail.com>
2024-06-10 16:45:41 -0400

    Playing with validation

--- MDF2ValidExcel.py
+++ MDF2ValidExcel.py
@@ -1,6 +1,7 @@
 import yaml
 from openpyxl import Workbook
 from openpyxl.worksheet.datavalidation import DataValidation
+from openpyxl.utils import get_column_letter
 import argparse
 
 def readYaml(yamlfile):
@@ -37,11 +38,21 @@ def main(args):
                 if 'Enum' in props['PropDefinitions'][prop]:
                     #There are enums let's write a data validation
                     #ws.cell(row = 2, column=col, value=props['PropDefinitions'][prop]['Enum'])
-                    print(props['PropDefinitions'][prop]['Enum'])
+                    #print(props['PropDefinitions'][prop]['Enum'])
                     #print(type(props['PropDefinitions'][prop]['Enum']))
-                    #dv = DataValidation(type='list', formula1=props['PropDefinitions'][prop]['Enum'])
+                    dv = DataValidation(type='list', formula1=props['PropDefinitions'][prop]['Enum'], allow_blank=True, showDropDown=True)
+                    column = get_column_letter(col)
+                    #maxrow = ws.max_row()
+                    maxrow = 1048576
+                    #colrange = column+'2:'+str(maxrow)
+                    colrange = "{}2:{}{}".format(column, column, str(maxrow))
+                    #print(colrange)
+                    #dv.range.append(colrange)
+                    dv.add(colrange)
+                    ws.add_data_validation(dv)
+
             col = col +1
-    print(args.outputfile)
+    #print(args.outputfile)
     wb.save(args.outputfile)
     wb.close()
         
@@ -54,3 +65,4 @@ if __name__ == "__main__":
         
     args = parser.parse_args()
     main(args)
+    
